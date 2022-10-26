80832550c69fe6e5861f9d9207b2e7454f1955e8
dav100ide <oreglio.dev@gmail.com>
2022-10-26 17:55:28 +0200

    chevron down ok

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 'use strict';
 const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
 const items = document.querySelector('.items');
-items.innerHTML += `<div class="item active"><img src="img/01.jpg"></div>`;
-for (let i = 1; i < images.length; i++) {
-   items.innerHTML += `<div class="item"><img src="${images[i]}"></div>`;
+for (let i = 0; i < images.length; i++) {
+   if (i === 0) {
+      items.innerHTML += `<div class="item active"><img src="${images[i]}"></div>`;
+   } else {
+      items.innerHTML += `<div class="item"><img src="${images[i]}"></div>`;
+   }
 }
 
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-// cliccando aggiungo classe active agli item
+
 let visible = 0;
 
-next.addEventListener('click', function () {});
+next.addEventListener('click', function () {
+   if (visible < images.length - 1) {
+      visible += 1;
+      console.log(visible);
+      document.querySelectorAll('.item')[visible - 1].classList.remove('active');
+      document.querySelectorAll('.item')[visible].classList.add('active');
+   }
+});
+
+console.log(visible + 'hjk');
+
+prev.addEventListener('click', function () {
+   if (visible < 0) {
+      visible -= 1;
+      console.log(visible);
+      document.querySelectorAll('.item')[visible + 1].classList.remove('active');
+      document.querySelectorAll('.item')[visible].classList.add('active');
+   }
+});
