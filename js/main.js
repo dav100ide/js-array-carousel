9ca249b12c66d5a40a1cbdb9214fc64719696c70
dav100ide <oreglio.dev@gmail.com>
2022-10-26 16:42:28 +0200

    punto 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
 const items = document.querySelector('.items');
-for (let i = 0; i < images.length; i++) {
+items.innerHTML += `<div class="item active"><img src="img/01.jpg"></div>`;
+for (let i = 1; i < images.length; i++) {
    items.innerHTML += `<div class="item"><img src="${images[i]}"></div>`;
 }
 
