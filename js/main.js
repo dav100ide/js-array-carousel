b85019a1b0fc78642ead316e075692232b217268
dav100ide <oreglio.dev@gmail.com>
2022-10-26 16:28:34 +0200

    commit hotwell

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 'use strict';
 const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
 const items = document.querySelector('.items');
-
 for (let i = 0; i < images.length; i++) {
    items.innerHTML += `<div class="item"><img src="${images[i]}"></div>`;
 }
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+// cliccando aggiungo classe active agli item
+let visible = 0;
+
+next.addEventListener('click', function () {});
